64e4eca67b1eb6e8d07468fa18625ca62986da1e
Caglar Kilic <73300418+CaglarKilic@users.noreply.github.com>
2024-04-15 11:19:06 +0300

    Added json-server for fake local db and added routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,5 +4,55 @@ export default [
   {
     path: "/",
     element: <App />,
+    id: "root",
+    loader: async () => {
+      const response = await fetch("http://localhost:3000/offers");
+      return response;
+    },
+    children: [
+      {
+        path: "create",
+        element: <CreateOffer />,
+      },
+      {
+        path: "offers",
+        element: <ViewOffers />,
+        action: async ({ request }) => {
+          const formData = await request.formData();
+          const response = await fetch("http://localhost:3000/offers", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(Object.fromEntries(formData)),
+          });
+          return response;
+        },
+      },
+      {
+        path: "offer/:id",
+        element: <Offer />,
+        loader: async ({ params }) => {
+          const response = await fetch(
+            `http://localhost:3000/offers/${params.id}`
+          );
+          return response;
+        },
+        action: async ({ request, params }) => {
+          const formData = await request.formData();
+          const response = await fetch(
+            `http://localhost:3000/offers/${params.id}`,
+            {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(Object.fromEntries(formData)),
+            }
+          );
+          return response;
+        },
+      },
+    ],
   },
 ];
